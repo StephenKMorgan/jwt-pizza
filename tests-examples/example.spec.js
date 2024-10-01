64e9133fffa13c08b61b313ccefdc4375f217f04
StephenKMorgan <stephen.morgan.dhi@gmail.com>
2024-10-01 16:09:14 -0600

    refactor: Update test imports to use 'playwright-test-coverage'

--- tests-examples/example.spec.js
+++ tests-examples/example.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from 'playwright-test-coverage';
+// const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
